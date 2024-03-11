620e30bb71020c2f56ae888fe7862e2564a3d65d
Jackson Gray <nohaxjustdoge@gmail.com>
2024-03-11 18:50:13 +0000

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linkElements = links.map((link, index)=>{
+    return <a href={`#${link}`} key={index}>{link}</a>
+  })
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {linkElements}
+  </nav>;
 }
 
 export default NavBar;
